71c7e2501ce7988109ddcb20b945918e8d9af731
Pavithra S <38652882+17uite035@users.noreply.github.com>
2024-01-13 17:42:04 +0530

    Create Product.java
    Ex 2-1.2

---
+++ Product.java
@@ -0,0 +1,37 @@
+public class Product {
+    private int pid;
+    private double price;
+    private int quantity;
+
+    // Parameterized constructor
+    public Product(int pid, double price, int quantity) {
+        this.pid = pid;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters and setters
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
